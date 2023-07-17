c94bc6177dde3d0ff2cb9d3371dc1e009e1a804e
osawa-koki <github@osawa.dev>
2023-07-18 08:41:05 +0900

    リスト。

---
+++ scripts/02.list.sh
@@ -0,0 +1,20 @@
+
+# リストの末尾に追加。
+RPUSH key [value ...]
+RPUSH mylist "Hello" "World" "!"
+
+# リストの先頭に追加。
+LPUSH <キー> [value ...]
+LPUSH mylist "Good" "Morning" "?"
+
+# リストの値を参照。
+LRANGE <キー> <開始インデックス> <終了インデックス>
+LRANGE mylist 0 -1
+
+# リストの末尾の値をポップ。
+RPOP <キー>
+RPOP mylist
+
+# リストの先頭の値をポップ。
+LPOP <キー>
+LPOP mylist
